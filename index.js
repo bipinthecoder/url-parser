68cde5a78e97c27d24c4eac8f9981a760096f4d8
Bipin Babu <bipinbabu@live.com>
2020-08-27 18:51:56 +0530

    Added few test cases and updated Readme

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-
-
-let inputURL = "foo://example.com:8042/over/there?name=ferret&&place=blr#nose";
-let obj = {}
+let inputURL = "foo://example.com:8042/over/there?name=ferret#nose";
+let obj = {} //declaring result object
 
 //Using URL() constructor of JS Web API
-var URLExec = new URL(inputURL);
+const URLExec = new URL(inputURL);
 
 let scheme = URLExec.protocol.split(":")[0];
 let domain = URLExec.hostname;
 let port = parseInt(URLExec.port);
-let path = URLExec.pathname.slice(1);
+let path = URLExec.pathname;
 let query = Array.from(URLExec.searchParams);
 let password = URLExec.password;
 let username = URLExec.username;
@@ -22,13 +20,40 @@ query && query.forEach(item => {
     queryArray.push(param);
 });
 
+//Filling the result object
 scheme && (obj["scheme"] = scheme);
 domain && (obj["domain"] = domain);
 port && (obj["port"] = port);
 path && (obj["path"] = path);
-query && (obj["query"] = queryArray);
+queryArray.length && (obj["query"] = queryArray);
 username && (obj["username"] = username);
 password && (obj["password"] = password);
 fragment && (obj["fragment"] = fragment);
 
 console.log(obj);
+
+//testcases
+/*
+  input  :: jdbc:mysql://test_user:ouupppssss@localhost:3306/sakila?profileSQL=true
+  output ::
+  { scheme: 'jdbc',
+  path: 'mysql://test_user:ouupppssss@localhost:3306/sakila',
+  query: [ 'profileSQL = true' ] }
+*/
+
+/*
+  input  :: urn:example:animal:ferret:nose
+  output ::
+  { scheme: 'urn', path: 'example:animal:ferret:nose' }
+*/
+
+/*
+  input  :: urn:example:animal:ferret:nose
+  output ::
+  { scheme: 'urn', path: 'example:animal:ferret:nose' }
+*/
+/*
+  input  :: urn:example:animal:ferret:nose
+  output ::
+  { scheme: 'urn', path: 'example:animal:ferret:nose' }
+*/
